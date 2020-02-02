4348474dfae28b701382ae9df8994c57f9de595e
Davitron <baizoualexandre@yahoo.fr>
2020-02-02 10:02:49 +0100

    chore(bootstrap): add basic config

---
+++ src/utils/error.filter.ts
@@ -0,0 +1,22 @@
+
+import { Injectable, ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+
+@Injectable()
+@Catch()
+export class ErrorFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const control = host.switchToHttp();
+    const response = control.getResponse();
+    const request = control.getRequest();
+
+    const { status: code, message: { error: name, message }} = exception;
+
+    const payload =  {
+      name: name || undefined,
+      code: code || null,
+      message: message || 'Internal Server Error',
+    };
+    response
+      .json({error: payload });
+  }
+}
