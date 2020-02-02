181ba6a235674bcf78150065d72da6c6e9d05919
Davitron <baizoualexandre@yahoo.fr>
2020-02-02 10:02:49 +0100

    chore(config): add basic config - add error filters - add response interceptors - implementd config modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './utils/response.interceptor';
+import { ErrorFilter } from './utils/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ErrorFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
